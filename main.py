6445ad9cb0e95fe4b2798a3e26182503ec1d2817
Dejan Serafimovski <serafimovskidejan@gmail.com>
2023-02-23 01:15:12 +0100

    Added comments and more clear code

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 #add function
-def add(a,b) : print("%.2f" % a,"+","%.2f" % b,"=","%.2f" % (a+b))
+def add(a,b) : return (a+b)
 
 #subtract function
-def sub(a,b) : print("%.2f" % a,"-","%.2f" % b,"=","%.2f" % (a-b))
+def sub(a,b) : return (a-b)
 
 #multiply function
-def multi(a,b) : print("%.2f" % a,"*","%.2f" % b,"=","%.2f" % (a*b))
+def multi(a,b) : return (a*b)
 
 #divide function
-def div(a,b) : print("%.2f" % a,"/","%.2f" % b,"=","%.2f" % (a/b))
-
+def div(a,b) : return (a/b)
  
-
 while True : 
     print("Select operation.\n" "1.Add\n" "2.Subtract\n" "3.Multiply\n" "4.Divide\n")
     print("Enter choice: (1/2/3/4):")
-    NumChoice = int(input())
+    #casting the number into integer because if not it will read it as string and the sring can't be compared with some operators
+    NumChoice = int(input()) 
+    #checking if the number is between 1 and 4
     if(NumChoice > 0 and NumChoice <=4) :
         print("Enter first number:")
+        #using casting to make sure the numbers are read in float
         a = float(input())
         print("Enter second number:")
         b = float(input())
-        if(NumChoice == 1) : add(a,b)
-        elif(NumChoice == 2) : sub(a,b)
-        elif(NumChoice == 3) : multi(a,b)
-        elif(NumChoice == 4) : div(a,b)
+        #checking what operation the user wants and shows them the result formated with 2 decimals with the specific function called for each operation
+        if(NumChoice == 1) : print("%.2f" % a,"+","%.2f" % b,"=","%.2f" % add(a,b))
+        elif(NumChoice == 2) : print("%.2f" % a,"-","%.2f" % b,"=","%.2f" % sub(a,b))
+        elif(NumChoice == 3) : print("%.2f" % a,"*","%.2f" % b,"=","%.2f" % multi(a,b))
+        elif(NumChoice == 4) : print("%.2f" % a,"/","%.2f" % b,"=","%.2f" % div(a,b))
         print("Would you like to do a new calculation ? (yes/no):")
         NewCalc = input()
+        #checking if the user wants to countinue or not
         if(NewCalc == "yes") : continue
-        elif(NewCalc == "no") : break
-    else : print("That is not a valid number !")
+        else : break
+    #if there is no valid number the program will terminate
+    else : print("That is not a valid number!")
     break
 
